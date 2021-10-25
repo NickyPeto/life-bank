e86c85911a2ed2db675c6304370425deab1c6fd6
NickyPeto <73256808+NickyPeto@users.noreply.github.com>
2021-10-25 19:58:02 +0200

    Initial folders and modules

---
+++ Components/ColorPallete.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+const colorPallete = StyleSheet.create({
+  mainColorPallete: {
+    color: "#8b31fd",
+    backgroundColor: "#f8f8f8",
+  },
+});
+
+export default colorPallete;
